94aa752fda9885f6555876b26bd0a14683367df2
Karol <karolroberta92@hotmail.com>
2021-06-14 19:34:05 -0300

    Criado primeiro cenario de teste

---
+++ src/test/java/config/Soc.java
@@ -0,0 +1,39 @@
+package config;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Soc {
+	String url;
+	WebDriver driver;
+
+	@Before
+	public void iniciar() {
+		url = " https://ww2.soc.com.br/blog/";
+
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30000, TimeUnit.MILLISECONDS);
+
+	}
+
+	@After
+	public void finalizar() {
+		driver.quit();
+	}
+
+	@Test
+	public void abrirPagina() {
+		driver.get(url);
+		assertEquals("https://ww2.soc.com.br/blog/.", driver.getCurrentUrl());
+	}
+
+}
---
+++ src/test/java/feature/BuscaNoBlog.feature
@@ -0,0 +1,11 @@
+#language: pt
+#Author: Karol Batista
+
+Funcionalidade: Busca no Blog
+Cenario: Pesquisar uma categoria
+Dado que o usuario que esteja no site soc tenha clicado no botao de pesquisa
+Quando busca por uma categoria "saude ocupacional" na barra de pesquisa
+E pressiona Enter
+Entao deve aparecer os resultados relacionados com a pesquisa
+Quando escolho "Saude Ocupacional"
+Entao exibe "Documentos legais"
